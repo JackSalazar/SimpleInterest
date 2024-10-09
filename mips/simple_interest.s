1c752f051e1936a3e70fd2cfa0cfa1e6a6e8d804
Jack Salazar <jack.salazar.649@my.csun.edu>
2024-10-09 11:28:58 -0700

    test

--- mips/simple_interest.s
+++ mips/simple_interest.s
@@ -1,7 +1,7 @@
         .text
-        .globl equation
+        .globl simple_interest
         .data
-        
+
          # SI = R / 100 * P * T;
          # where
          #   P  = Principal, i.e., your investment
@@ -15,7 +15,7 @@
         #P = a0
         #R = a1
         #T = a2
-    equation: nop                  #public static int simple_interest(int P, int R, int T) {
+    simple_interest: nop                  #public static int simple_interest(int P, int R, int T) {
                                      #int SI;
                                      #
                                      #int t1;
@@ -23,11 +23,15 @@
                                      #
                                      ## Insert your code here
                                      ##SI = R  * P * T / 100; 
+        li $t0, 100
         mul $t1, $a1, $a0                             #t1 = R * P;
+        #print_d $a1
+        #print_d
         mul $t1, $t1, $a2                             #t1 = t1 * T;
-        div $v0, $t1, 100                             #SI = t1 / 100;
-                                     #
-        print_d($v0)                            #mips.print_d(SI);
-        print_ci('\n')                             #mips.print_ci('\n');
+        div $t1, $t1, $t0                             #SI = t1 / 100;
+        move $v0, $t1                             #
+        
+        print_d $t1                          #mips.print_d(SI);
+        print_ci '\n'                              #mips.print_ci('\n');
                                      # return 0;
                           #}
